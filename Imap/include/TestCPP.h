6903b1d54e7448e631d99640595bc629681edb6a
Shawn Button <smb1717@rit.edu>
2013-01-10 01:51:17 -0500

    Initial commit with my code block project

---
+++ Imap/include/TestCPP.h
@@ -0,0 +1,14 @@
+#ifndef TESTCPP_H
+#define TESTCPP_H
+#include <iostream>
+
+class TestCPP
+{
+    public:
+        TestCPP();
+        virtual ~TestCPP();
+    protected:
+    private:
+};
+
+#endif // TESTCPP_H
---
+++ Imap/src/TestCPP.cpp
@@ -0,0 +1,18 @@
+#include "C:\Users\Kabaniz\Desktop\DataCommProj2\DataCommProject\Imap\\include\TestCPP.h"
+using namespace std;
+
+TestCPP::TestCPP()
+{
+    //ctor
+}
+
+TestCPP::~TestCPP()
+{
+    //dtor
+}
+
+int main ()
+{
+  cout << "Hello World!";
+  return 0;
+}
